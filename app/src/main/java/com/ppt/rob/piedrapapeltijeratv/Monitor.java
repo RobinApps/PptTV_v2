67682433471b6511cd476c956501bea2d28274a5
edgarvega <Huberto1739@gmail.com>
2016-09-16 22:08:18 -0600

    primeras funcionalidades

---
+++ app/src/main/java/com/ppt/rob/piedrapapeltijeratv/Monitor.java
@@ -0,0 +1,16 @@
+package com.ppt.rob.piedrapapeltijeratv;
+
+/**
+ * Created by Huberto on 16/09/2016.
+ */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Monitor extends BroadcastReceiver{
+    public void onReceive(Context context,Intent intent) {
+        Intent i = new Intent(context, Principal.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
--- app/src/main/java/com/ppt/rob/piedrapapeltijeratv/Principal.java
+++ app/src/main/java/com/ppt/rob/piedrapapeltijeratv/Principal.java
@@ -1,9 +1,21 @@
 package com.ppt.rob.piedrapapeltijeratv;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Principal extends AppCompatActivity {
+    Principal thisActivity;
+    VideoView videoView;
 //En esta Activity mostra un video full pantalla vertical, y en la parte inferior aparesca el código generado aleatoriamente despues de cada juego
     //y que al momento de recibir una conexión de algun jugador cambie de activity para iniciar el juego
     //tambien create el servicio para que inicie la app al momento de iniciar el dispositivo movil.
@@ -11,5 +23,32 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        thisActivity = this;
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // ----------------------Hide code
+        try{getActionBar().hide();}catch(Exception e){}
+        // ----------------------/Hide code
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        videoView = (VideoView)findViewById(R.id.videoView);
+
+        String uriPath = "android.resource://"+getPackageName()+"/raw/video";
+        Uri uri = Uri.parse(uriPath);
+        videoView.setVideoURI(uri);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                if(!thisActivity.isFinishing()){
+                    /*Intent intent = new Intent(thisActivity, Main.class);
+                    thisActivity.startActivity(intent);
+                    thisActivity.finish();*/
+                    videoView.start();
+                }
+            }
+        });
+        videoView.start();
+
     }
 }
