d80d525f06120942e50329059f14801a50f33572
RobinApps <robin_skt555@hotmail.com>
2016-09-16 18:53:53 -0600

    commit descripción

--- app/src/main/java/com/ppt/rob/piedrapapeltijeratv/Principal.java
+++ app/src/main/java/com/ppt/rob/piedrapapeltijeratv/Principal.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class Principal extends AppCompatActivity {
 //En esta Activity mostra un video full pantalla vertical, y en la parte inferior aparesca el código generado aleatoriamente despues de cada juego
     //y que al momento de recibir una conexión de algun jugador cambie de activity para iniciar el juego
-    //tambien create el servicio para que inicie la app al momento de iniciar el dispositivo movil
+    //tambien create el servicio para que inicie la app al momento de iniciar el dispositivo movil.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
